81490d36809ee7c2354636f074b189f9d24c2102
mohitpatil30501 <mohitpatil30501@gmail.com>
2021-02-19 22:57:06 +0530

    19-02-2021 / Dashboard template work done Except: File and Photo

--- Dashboard/urls.py
+++ Dashboard/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
+    path('edit/details', views.edit_details, name='edit_details'),
+    path('edit/designation', views.edit_designation, name='edit_designation'),
+    path('edit/activity', views.edit_activity, name='edit_activity'),
 ]
--- Dashboard/views.py
+++ Dashboard/views.py
@@ -1,5 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def dashboard(request):
-    return render(request, "Dashboard/dashboard.html")
+    if request.user.is_authenticated:
+        return render(request, "Dashboard/dashboard.html")
+    return redirect('/')
+
+
+def edit_details(request):
+    if request.user.is_authenticated:
+        return render(request, "Dashboard/details.html")
+    return redirect('/')
+
+
+def edit_designation(request):
+    if request.user.is_authenticated:
+        return render(request, "Dashboard/designation.html")
+    return redirect('/')
+
+
+def edit_activity(request):
+    if request.user.is_authenticated:
+        return render(request, "Dashboard/activity.html")
+    return redirect('/')
